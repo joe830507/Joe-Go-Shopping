2cf0f7f48cba878bff9604208f17e28b10f57ba9
JiaHongLin <joe830507@gmail.com>
2022-03-07 13:38:50 +0800

    initialization

---
+++ src/main/java/com/lin/app/JoeJavaGroceryStoreApplication.java
@@ -0,0 +1,13 @@
+package com.lin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoeJavaGroceryStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoeJavaGroceryStoreApplication.class, args);
+	}
+
+}
