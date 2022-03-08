44d2e1c515130da04dce2dcf49eaeb4cc246b831
JiaHongLin <joe830507@gmail.com>
2022-03-08 15:39:38 +0800

    Create MongoConfig.java
    add a configuration independently to enable mongo repositories

---
+++ src/main/java/com/lin/configs/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.lin.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "com.lin.repository")
+public class MongoConfig {
+}
