0d877a2ebddfc5cf123eefc36885e6e6545c83f9
JiaHongLin <joe830507@gmail.com>
2022-03-08 15:36:46 +0800

    Update JoeJavaGroceryStoreApplication.java
    add the path for scanning - all classes with annotation such as @Configuration, @RestController, @Component, @Service will be instanced and under Spring's control.

--- src/main/java/com/lin/app/JoeJavaGroceryStoreApplication.java
+++ src/main/java/com/lin/app/JoeJavaGroceryStoreApplication.java
@@ -3,7 +3,7 @@ package com.lin.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.lin")
 public class JoeJavaGroceryStoreApplication {
 
 	public static void main(String[] args) {
