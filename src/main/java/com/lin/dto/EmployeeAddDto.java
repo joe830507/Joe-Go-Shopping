6440f12bfb453829da73e1fc6df82bb1e55824fb
JiaHongLin <joe830507@gmail.com>
2022-03-08 15:47:51 +0800

    new dto about employee

---
+++ src/main/java/com/lin/dto/EmployeeAddDto.java
@@ -0,0 +1,11 @@
+package com.lin.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeeAddDto {
+	private String firstName;
+	private String lastName;
+	private String account;
+	private String password;
+}
---
+++ src/main/java/com/lin/dto/EmployeeUpdateDto.java
@@ -0,0 +1,11 @@
+package com.lin.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeeUpdateDto {
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String password;
+}
