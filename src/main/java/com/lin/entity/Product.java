e1e3ece45f7162953fdc6824a1982e0e33b1ae01
JiaHongLin <joe830507@gmail.com>
2022-03-31 10:42:08 +1100

    Update Product.java

--- src/main/java/com/lin/entity/Product.java
+++ src/main/java/com/lin/entity/Product.java
@@ -1,5 +1,16 @@
 package com.lin.entity;
 
-public class Product {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class Product {
+	private String id;
+	private String name;
+	private Float number;
+	private Float importPirce;
+	private Float salesPrice;
+	private Date createTime;
+	private Date updateTime;
 }
